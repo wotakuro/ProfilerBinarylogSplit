72093f04e0a3883f310e706ae7dbb5ef3f55aafc
Yusuke Kurokawa <wotakuro@gmail.com>
2018-09-12 17:37:29 +0900

    update 2017.3 or newer .log.data

--- Assets/ProfilerBinlogSplit/Editor/BinLogToUnityProfiler.cs
+++ Assets/ProfilerBinlogSplit/Editor/BinLogToUnityProfiler.cs
@@ -10,15 +10,22 @@ using UnityEngine.Profiling;
 
 namespace ProfilerBinlogSplit
 {
+
+    interface ILogFileSlicer
+    {
+        int GetCurrentFrame();
+        bool CreateTmpFile(int frameNum, string tmpFile);
+    }
+
     public class BinLogToUnityProfiler : EditorWindow
     {
         private const string TmpFileName = "tmp.log";
         private string filePath;
-        private int currentFrame = 0;
-        private long currentFilePos = 0;
         private int readFrameNum = 100;
 
-        [MenuItem("Tools/ProfilerBinlogSplit")]
+        private ILogFileSlicer slicer;
+
+        [MenuItem("Tools/ProfilerBinlogSlice")]
         public static void GetWindow()
         {
             EditorWindow.GetWindow<BinLogToUnityProfiler>();
@@ -33,12 +40,11 @@ namespace ProfilerBinlogSplit
             this.filePath = EditorGUILayout.TextField(this.filePath);
             if (GUILayout.Button("File", GUILayout.Width(40.0f)))
             {
-                this.filePath = EditorUtility.OpenFilePanel("", "Select BinaryLogFile", "data");
+                this.filePath = EditorUtility.OpenFilePanelWithFilters("", "Select BinaryLogFile", new string[] { "profiler log", "data,raw" });
             }
             if (oldFile != this.filePath)
             {
-                this.currentFrame = 0;
-                this.currentFilePos = 0;
+                slicer = null;
             }
             EditorGUILayout.EndHorizontal();
 
@@ -49,7 +55,18 @@ namespace ProfilerBinlogSplit
             {
                 try
                 {
-                    bool flag = CreateTmpFile(readFrameNum);
+                    if(slicer == null)
+                    {
+                        if (RawDataFileSlicer.IsRawData(this.filePath))
+                        {
+                            slicer = new RawDataFileSlicer(this.filePath);
+                        }
+                        else
+                        {
+                            slicer = new LogDataFileSlicer(this.filePath);
+                        }
+                    }
+                    bool flag = slicer.CreateTmpFile(readFrameNum,TmpFileName);
                     if (flag)
                     {
                         Profiler.AddFramesFromFile(TmpFileName);
@@ -57,52 +74,16 @@ namespace ProfilerBinlogSplit
                 }
                 catch (System.Exception ed) { }
             }
-            GUILayout.Label("Frame " + currentFrame);
-            if (GUILayout.Button("Reset Frame"))
+            if (slicer != null)
             {
-                this.currentFrame = 0;
-                this.currentFilePos = 0;
-            }
-
-        }
-
-        private bool CreateTmpFile(int frameNum)
-        {
-            bool flag = false;
-            using (FileStream fs = File.OpenRead(this.filePath))
-            {
-                using (FileStream writeFs = File.Open(TmpFileName + ".data", FileMode.Create))
+                GUILayout.Label("Frame " + slicer.GetCurrentFrame());
+                if (GUILayout.Button("Reset Frame"))
                 {
-                    if (currentFilePos != 0)
-                    {
-                        fs.Seek(currentFilePos, SeekOrigin.Begin);
-                    }
-                    for (int i = 0; i < frameNum; ++i)
-                    {
-                        if (fs.Length <= fs.Position) { break; }
-                        byte[] header = new byte[16];
-                        fs.Read(header, 0, header.Length);
-                        int size = GetIntValue(header, 8);
-                        writeFs.Write(header, 0, header.Length);
-                        byte[] buffer = new byte[size];
-                        fs.Read(buffer, 0, size);
-                        writeFs.Write(buffer, 0, buffer.Length);
-                        currentFilePos += size + 16;
-                        currentFrame++;
-                        flag = true;
-                    }
+                    slicer = null;
                 }
             }
-            return flag;
-        }
 
-        private static int GetIntValue(byte[] bin, int offset)
-        {
-            return (bin[offset + 0] << 0) +
-                (bin[offset + 1] << 8) +
-                (bin[offset + 2] << 16) +
-                (bin[offset + 3] << 24);
         }
-
+        
     }
 }
---
+++ Assets/ProfilerBinlogSplit/Editor/RawDataFileSlicer.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ProfilerBinlogSplit
+{
+    public class RawDataFileSlicer : ILogFileSlicer
+    {
+        const uint FileSignature = 'U' << 24 | '3' << 16 | 'D' << 8 | 'P';
+        const uint BlockHeaderSignature = 0xB10C7EAD;
+        const ulong BlockHeaderGlobalThreadId = 18446744073709551614;
+
+        private ulong mainThreadId;
+        private uint version;
+        private string currentFilePath;
+        private long currentFilePos;
+        private int currentFrame;
+
+        private MemoryStream globalDataStream;
+        private MemoryStream currentDataStream;
+
+        public static bool IsRawData(string path)
+        {
+            var bin = ReadFromFile(path, 0, 4);
+            uint header = GetUInt(bin, 0);
+            return (FileSignature == header);
+        }
+
+        public RawDataFileSlicer(string path)
+        {
+            currentFilePath = path;
+            globalDataStream = new MemoryStream(1024*1024);
+            currentDataStream = new MemoryStream(1024 * 1024);
+            // read header
+            byte[] fileHeader = ReadFromFile(path, 0, 36);
+            version = GetUInt(fileHeader, 8);
+            mainThreadId = GetULongValue(fileHeader, 28);
+
+            // append GlobalData
+            globalDataStream.Write(fileHeader, 0, fileHeader.Length);
+            this.currentFilePos = fileHeader.Length;
+        }
+
+
+        public int GetCurrentFrame()
+        {
+            return currentFrame;
+        }
+
+
+        public bool CreateTmpFile(int frameNum, string tmpFile)
+        {
+            try
+            {
+                for (int i = 0; i < 10; ++i) { this.ReadBlock(); }
+                using (FileStream writeFs = File.Open(tmpFile + ".data", FileMode.Create))
+                {
+                    writeFs.Write(globalDataStream.ToArray(), 0, (int)globalDataStream.Length);
+                    writeFs.Write(currentDataStream.ToArray(), 0, (int)currentDataStream.Length);
+                }
+                if (currentDataStream != null) {
+                    currentDataStream.Close();
+                    currentDataStream = new MemoryStream();
+                }
+
+                UnityEngine.Debug.Log("globalDataStream " + globalDataStream.Length);
+                UnityEngine.Debug.Log("currentDataStream " + currentDataStream.Length);
+                currentFrame += frameNum;
+            }catch(System.Exception e)
+            {
+                UnityEngine.Debug.LogError(e);
+            }
+            return true;
+        }
+
+        private void ReadBlock()
+        {
+            byte[] blockHeader = ReadFromFile(currentFilePath, this.currentFilePos , 20);
+            ulong threadId = GetULongValue(blockHeader, 8);
+            uint length = GetUInt(blockHeader, 16);
+            this.currentFilePos += 20;
+
+            byte[] blockBodyAndFooter = ReadFromFile(currentFilePath, this.currentFilePos, (int)length + 8);
+
+            this.currentFilePos += length;
+            this.currentFilePos += 8;
+
+            if ( threadId == BlockHeaderGlobalThreadId)
+            {
+                globalDataStream.Write(blockHeader, 0, blockHeader.Length);
+                globalDataStream.Write(blockBodyAndFooter, 0, blockBodyAndFooter.Length);
+            }
+            currentDataStream.Write(blockHeader, 0, blockHeader.Length);
+            currentDataStream.Write(blockBodyAndFooter, 0, blockBodyAndFooter.Length);
+        }
+
+
+        private static uint GetUInt(byte[] data,int offset)
+        {
+            uint val = ((uint)data[offset+0] << 0) + 
+                ((uint)data[offset + 1] << 8) + 
+                ((uint)data[offset + 2] << 16) + 
+                ((uint)data[offset + 3] << 24);
+            return val;
+        }
+
+        public static ulong GetULongValue(byte[] bin, int offset)
+        {
+            return (ulong)(bin[offset + 0] << 0) +
+                (ulong)(bin[offset + 1] << 8) +
+                (ulong)(bin[offset + 2] << 16) +
+                (ulong)(bin[offset + 3] << 24) +
+                (ulong)(bin[offset + 4] << 32) +
+                (ulong)(bin[offset + 5] << 40) +
+                (ulong)(bin[offset + 6] << 48) +
+                (ulong)(bin[offset + 7] << 56);
+        }
+        private static byte[] ReadFromFile( string file , long offset,int length)
+        {
+            byte[] data = new byte[length];
+
+            using (FileStream fs = File.OpenRead(file))
+            {
+                fs.Position = offset;
+                int read = fs.Read(data, 0, length);
+                if( read < length) { throw new System.Exception("ReadError " + length + " - " + read); }
+                fs.Close();
+            }
+
+            return data;
+
+        }
+
+    }
+}
